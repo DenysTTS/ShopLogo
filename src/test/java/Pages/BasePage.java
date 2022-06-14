2380888ceada61529425e260aa0d22f4d5b7b5d6
DenysTXJ <denyst89@gmail.com>
2022-06-14 16:35:24 -0400

    BasePageCreated LocatorsAdded

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,4 +1,21 @@
 package Pages;
 
+import Utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(linkText = "Sign in")
+    public WebElement SignInButton;
+
+    @FindBy(linkText = "Cart")
+    public WebElement ShoppingCart;
+
+    @FindBy(id = "search_query_top")
+    public WebElement SearchBar;
 }
